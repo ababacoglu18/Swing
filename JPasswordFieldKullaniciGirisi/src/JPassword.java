416c3dc185c80b2ce13dd8ce5783b5b7f811f317
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-12-28 01:04:57 +0300

    PasswordFinal

--- JPasswordFieldKullaniciGirisi/src/JPassword.java
+++ JPasswordFieldKullaniciGirisi/src/JPassword.java
@@ -15,8 +15,48 @@ public class JPassword {
         girisYapButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
+                String kullanici_adi = kullaniciAdiAlani.getText();
+                String parola = new String(parolaAlani.getPassword());
+
+                String message;
+
+                if(kullanici_adi.equals("fazilsay") && parola.equals("12345")){
+                    System.out.println("Lutfen Bekleyiniz.");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    message = "Hosgeldiniz " + kullanici_adi ;
+                }
+               else if(!kullanici_adi.equals("fazilsay") && parola.equals("1234")){
+                    System.out.println("Lutfen Bekleyiniz.");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    message = "kullanici adi veya parola yanlis ama hangisi soylemem ";
+                }
+               else if(kullanici_adi.equals("fazilsay") && !parola.equals("1234")){
+                    System.out.println("Lutfen Bekleyiniz.");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+
+                    message = "Parola Yanlış.";
+                }else {
+                   message = " yok ki oyle bisey";
+                }
+
+                JOptionPane.showMessageDialog(null,message);
+               kullaniciAdiAlani.setText("");
+               parolaAlani.setText("");
+               System.exit(1);
             }
+
         });
     }
 
