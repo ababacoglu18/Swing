cb96c7f5b13cbbf829f92ec12e6f832faed6cb9c
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-12-29 21:13:35 +0300

    SetSelectedIndex

--- InputDialogJList/src/JListInputDialog.java
+++ InputDialogJList/src/JListInputDialog.java
@@ -3,14 +3,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class JListInputDialog {
-    public JListInputDialog(DefaultListModel model) {
-        this.model = (DefaultListModel) kullaniciListesi.getModel();  //CONSTRUCTORDA JLIST CONVERT EDILMİYOR ARASTIR.
-    }
-
-    DefaultListModel model;
 
 
+    DefaultListModel model = new DefaultListModel();//modeli başlatmazsan aşağıya null gider Exception verir
+                                                        //2 saatte çözüldü :S
+    /*public JListInputDialog(DefaultListModel model) {
+        kullaniciListesi.setModel(model); //CONSTRUCTORDA JLIST CONVERT EDILMİYOR ARASTIR. Her metodun içine yazmak zorundayım.
+    }*/
 
+    private int listeSayaci = 1;
     private JList kullaniciListesi;
     private JPanel panel1;
 
@@ -27,6 +28,8 @@ public class JListInputDialog {
         addbuton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                kullaniciListesi.setModel(model);               //Convert'u burda yapmalisin.
+
 
                 mesajBox.setText("");
                 String cevap = JOptionPane.showInputDialog("Kullanıcı İsmi");
@@ -35,9 +38,36 @@ public class JListInputDialog {
                         mesajBox.setText("Boş Bir Kullanıcı İsmi Girdiniz. ");
 
                     }else{
-                        model.addElement(cevap);
+                        model.addElement(  listeSayaci+". "+cevap);
+
+                        mesajBox.setText("Kullanıcı Başarılı Bir Şekilde Eklendi. ");
+                        listeSayaci++;
                     }
+                }else {
+                    mesajBox.setText("İşlem İptal Edildi.");
                 }
+
+            }
+        });
+        deletebuton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mesajBox.setText("");
+               // kullaniciListesi.setModel(model);   //2 KERE MODEL SETLEME INDEXİ -1 E ÇEVİRİYOR.
+                int selectedIndex = kullaniciListesi.getSelectedIndex(); //Önemli -1 boş
+
+                if(selectedIndex == -1){
+                    if (model.getSize()==0){
+                        mesajBox.setText("Liste Şu Anda Boş, Değer Silinemez.");
+                    }else {
+                        mesajBox.setText("Lütfen Silinecek Bir Kullanıcı Seçiniz. ");
+                    }
+                }else {
+                    Object silinecek= model.getElementAt(selectedIndex);
+                    model.removeElement(model.getElementAt(selectedIndex)); //Object Referans İstiyor.
+                }
+
+
 
             }
         });
