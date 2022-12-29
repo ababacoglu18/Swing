56b8a5d78503072fa827b39762d32e09c37f38ce
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-12-29 19:11:56 +0300

    d

--- JComboBoxandRadioButton/src/Main.java
+++ JComboBoxandRadioButton/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-
-    //safklmflkasmdfkl
+//sffsdfdffffffffffffffffffffffff
+    //aa
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
--- StokUygulamasi/src/StokUygulamasi.java
+++ StokUygulamasi/src/StokUygulamasi.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class StokUygulamasi {
     private JPanel StokPanel;
@@ -13,6 +15,7 @@ public class StokUygulamasi {
     private JScrollPane scrollpane;
     private JButton urunEkleButton;
     private JButton urunGuncelleButton;
+    private JButton urunSilButton;
 
     public StokUygulamasi() {
         urunEkleButton.addActionListener(new ActionListener() {
@@ -60,6 +63,37 @@ public class StokUygulamasi {
                     model.setValueAt(tf_fiyat.getText(),secili_row,2);
                 }
 
+            }
+        });
+        urunTablosu.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                DefaultTableModel model = (DefaultTableModel) urunTablosu.getModel();
+                int secili_row = urunTablosu.getSelectedRow();
+
+                tf_urun.setText(model.getValueAt(secili_row,0).toString());             //Tıklanılan Row'u yukardaki bosluga yazar
+                cb_kategori.setSelectedItem(model.getValueAt(secili_row,1).toString());
+                tf_fiyat.setText(model.getValueAt(secili_row,2).toString());
+
+            }
+        });
+        urunSilButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mesaj_yazisi.setText("");
+                DefaultTableModel model = (DefaultTableModel) urunTablosu.getModel();
+                int secili_row = urunTablosu.getSelectedRow();
+                if(secili_row == -1){
+
+                    if(secili_row ==0){
+                        mesaj_yazisi.setText("Ürün Tablosu Şu Anda Boş.");
+                    }
+                   else  {
+                        mesaj_yazisi.setText("Lutfen Silinecek Ürünü Seçiniz");
+                    }
+                }else {
+                model.removeRow(urunTablosu.getSelectedRow());
+                }
             }
         });
     }
